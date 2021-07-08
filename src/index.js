82d4f2e52b03e27aeacd5dc63f1b9d78dbc9a698
WSGokou <gokou097@gmail.com>
2021-07-08 15:41:13 +0100

    add css, sass, postcss, hot reloading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+import "./styles/index.scss";
 const elvenShieldRecipe = {
   leatherStrips: 2,
   ironIngot: 1,
   refinedMoonstone: 4,
-}
+};
 
 const elvenGauntletsRecipe = {
   ...elvenShieldRecipe,
   leather: 1,
   refinedMoonstone: 5,
-}
-console.log(elvenShieldRecipe)
+};
+console.log(elvenShieldRecipe);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,43 @@
-let mode = "development"
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let mode = "development";
+let target = "web";
 
 if (process.env.mode === "production") {
   mode = "production";
+  target = "browserslist";
 }
 
 module.exports = {
   mode: mode,
+  target: target,
+
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      }
-    }]
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
+
+  plugins: [new MiniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
-    contentBase: "./dist"
-  }
-}
+    contentBase: "./dist",
+    hot: true,
+  },
+};
